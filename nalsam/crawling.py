44d8d0951fdc3c2156d9999dbfcc0d0e0ac324c8
lecto17 <khwoo_rpsit@naver.com>
2020-12-16 01:54:08 +0900

    주석 및 구절, 질문 내용까지 크롤링하도록 수정

--- nalsam/crawling.py
+++ nalsam/crawling.py
@@ -19,8 +19,16 @@ for d in days.split("\n"):
     day += d+". "
 #제목&본문
 prefix = driver.find_element_by_id("qtDayText2").text
+#본문 말씀
+bible_script = driver.find_element_by_class_name("qtBox").text
+#주석
+description = driver.find_element_by_class_name("script").text
 #길잡이
 ending = driver.find_elements_by_class_name("box2Content")[1].text
+#질문
+question_list = driver.find_element_by_xpath("//*[@id='content']/p[2]").text
+
+
 
 f = open('C:/Users/hyunoos/Desktop/QT.txt', 'ab')
 
@@ -32,7 +40,16 @@ print("*****TITLE&본문******")
 print("제목: "+prefix.split('\n')[0])
 content += prefix.split('\n')[0]+"\n"
 print("본문: "+prefix.split('\n')[1], end="\n\n")
-content += prefix.split('\n')[1]+"\n\n\n\n"
+content += prefix.split('\n')[1]+"\n\n"
+print("*****본문 구절*****")
+print(bible_script+"\n")
+content += bible_script+"\n\n"
+print("*****주석*****")
+print(description+"\n")
+content += description+"\n\n"
+print("*****질문*****")
+print(question_list)
+content += question_list+"\n\n"
 
 print("*****ENDING******")
 print(ending)
@@ -60,4 +77,4 @@ f.close()
 #link 걸어놓은 주소 중 해당하는 element를 반환
 #driver.find_element_by_xpath('//a[@href="'++'"]')
 
-driver.close()
+# driver.close()
